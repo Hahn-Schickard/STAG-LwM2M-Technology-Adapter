ea0d8f5b5556942b1a88610ea9cd584f0c8e499d
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-03-04 14:19:55 +0100

    bump LwM2M_Server to 0.2.0

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -10,7 +10,7 @@ class PackageConan(ConanFile):
     topics = ("conan", "stag", "modelling", "lwm2m", "technology-adapter")
     build_requires = "gtest/1.10.0"
     requires = [
-        "Technology_Adapter_Interface/0.1.4@hahn-schickard/stable",
+        "Technology_Adapter_Interface/0.2.0@hahn-schickard/stable",
         "Variant_Visitor/0.1.0@hahn-schickard/stable",
         "LwM2M_Server/0.1.2@hahn-schickard/stable"
     ]

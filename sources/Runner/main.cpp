a3d70c03db195548ec690f05d7aac5a184eb3d63
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-03-18 12:28:49 +0100

    use nice mocks to supress unintresting calls from gtest

--- sources/Runner/main.cpp
+++ sources/Runner/main.cpp
@@ -11,14 +11,16 @@ using namespace Technology_Adapter::testing;
 using namespace Information_Model::testing;
 using namespace std;
 
+using ::testing::NiceMock;
+
 int main(int argc, const char *argv[]) {
   try {
     LoggerRepository::initialise("config/loggerConfig.json");
     LoggerRepository::getInstance().configure(SeverityLevel::TRACE);
     auto adapter = make_shared<Technology_Adapter::LwM2M_TechnologyAdapter>(
         "config/serverConfig.json");
-    adapter->setInterfaces(make_shared<DeviceBuilderInterfaceMock>(),
-                           make_shared<ModelRegistryMock>());
+    adapter->setInterfaces(make_shared<NiceMock<DeviceBuilderInterfaceMock>>(),
+                           make_shared<NiceMock<ModelRegistryMock>>());
     adapter->start();
 
     if (argc > 1) {

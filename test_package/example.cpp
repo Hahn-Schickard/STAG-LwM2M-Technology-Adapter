3c8329705ecf28715e67147e21ec147d27f07a25
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-11-17 17:20:21 +0100

    ad exception handling and success/failure messages to integration test

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -2,6 +2,8 @@
 #include "HaSLL/SPD_LoggerRepository.hpp"
 #include "LwM2M_Technology_Adapter/LwM2M_Adapter.hpp"
 
+#include <exception>
+#include <iostream>
 #include <memory>
 
 using namespace HaSLL;
@@ -9,12 +11,19 @@ using namespace std;
 using namespace Technology_Adapter;
 
 int main() {
-  auto repo = make_shared<SPD_LoggerRepository>("config/loggerConfig.json");
-  LoggerManager::initialise(repo);
   try {
+    auto repo = make_shared<SPD_LoggerRepository>("config/loggerConfig.json");
+    LoggerManager::initialise(repo);
+
     make_shared<LwM2M_TechnologyAdapter>("config/serverConfig.json");
+
+    cout << "Integration test successful." << endl;
+    exit(EXIT_SUCCESS);
   } catch (const exception& ex) {
+    cout << "An unhandled exception occurred during integration test. "
+            "Exception: "
+         << ex.what() << endl;
+    cout << "Integration test failed" << endl;
     exit(EXIT_FAILURE);
   }
-  exit(EXIT_SUCCESS);
 }

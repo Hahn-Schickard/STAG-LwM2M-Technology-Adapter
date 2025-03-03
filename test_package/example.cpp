08a568c438d8da0d4c4ec64ebc3168ec570231e7
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-03-03 17:25:58 +0100

    refactor to use latest HaSLL

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,5 +1,4 @@
 #include "HaSLL/LoggerManager.hpp"
-#include "HaSLL/SPD_LoggerRepository.hpp"
 #include "LwM2M_Technology_Adapter/LwM2M_Adapter.hpp"
 
 #include <exception>
@@ -11,25 +10,26 @@ using namespace std;
 using namespace Technology_Adapter;
 
 int main() {
+  auto status = EXIT_SUCCESS;
   try {
-    auto repo = make_shared<SPD_LoggerRepository>("config/loggerConfig.json");
-    LoggerManager::initialise(repo);
+    LoggerManager::initialise(
+        makeDefaultRepository("config/loggerConfig.json"));
 
-    auto adapter =
-        make_shared<LwM2M_TechnologyAdapter>("config/serverConfig.json");
     try {
+      auto adapter =
+          make_shared<LwM2M_TechnologyAdapter>("config/serverConfig.json");
       adapter->start();
       cerr << "Adapter started without interfaces being set" << endl;
-      exit(EXIT_FAILURE);
+      status = EXIT_FAILURE;
     } catch (const exception& ex) {
       cout << "Integration test successful." << endl;
-      exit(EXIT_SUCCESS);
     }
-  } catch (const exception& ex) {
-    cerr << "An unhandled exception occurred during integration test. "
-            "Exception: "
-         << ex.what() << endl;
+    LoggerManager::terminate();
+  } catch (...) {
+    cerr << "An unknown unhandled exception occurred during integration test. "
+         << endl;
     cerr << "Integration test failed" << endl;
-    exit(EXIT_FAILURE);
+    status = EXIT_FAILURE;
   }
+  exit(status);
 }
